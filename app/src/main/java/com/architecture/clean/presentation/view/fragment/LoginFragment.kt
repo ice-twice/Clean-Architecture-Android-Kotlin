dc8e676ac1297f9cd1f7b78e74d54f72881e24f6
awecea <awecea@gmail.com>
2018-10-08 21:55:37 +0300

    Delete redundant code.

--- app/src/main/java/com/architecture/clean/presentation/view/fragment/LoginFragment.kt
+++ app/src/main/java/com/architecture/clean/presentation/view/fragment/LoginFragment.kt
@@ -48,30 +48,20 @@ class LoginFragment : BaseFragment(), LoginView {
 
 
     override fun showLoading() {
-
-        println("qwerty showLoading()")
-
         login_progress.visibility = View.VISIBLE
         login_form.visibility = View.GONE
     }
 
     override fun hideLoading() {
-        println("qwerty hideLoading()")
-
-
         login_progress.visibility = View.GONE
         login_form.visibility = View.VISIBLE
     }
 
     override fun showLoginError() {
-        println("qwerty showLoginError()")
-
         Toast.makeText(context, "Login failed!", Toast.LENGTH_LONG).show()
     }
 
     override fun showMain() {
-        println("qwerty showMain()")
-
         //show main
     }
 }
