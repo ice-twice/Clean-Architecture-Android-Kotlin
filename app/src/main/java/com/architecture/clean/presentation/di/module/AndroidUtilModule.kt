db26b904fe982ffcd1aca3cbb577d05f335d78a1
awecea <awecea@gmail.com>
2018-10-24 20:45:24 +0300

    Add a soft reference to the util module.

--- app/src/main/java/com/architecture/clean/presentation/di/module/AndroidUtilModule.kt
+++ app/src/main/java/com/architecture/clean/presentation/di/module/AndroidUtilModule.kt
@@ -3,20 +3,28 @@ package com.architecture.clean.presentation.di.module
 import com.architecture.clean.presentation.util.AndroidUtil
 import dagger.Module
 import dagger.Provides
+import java.lang.ref.SoftReference
 import javax.inject.Singleton
 
 /**
  * The module to provide utils.
+ *
+ * A soft reference is used to provide the same object for all clients and prevent creating redundant instances. This is like singleton behaviour but a held object can be removed if there are no references to it.
  */
 @Module
 class AndroidUtilModule {
-    private val androidUtils by lazy(mode = LazyThreadSafetyMode.NONE) {
-        AndroidUtil()
+    object Holder {
+        var androidUtils: SoftReference<AndroidUtil?> = SoftReference(null)
     }
 
     @Provides
     @Singleton
     internal fun provideAndroidUtil(): AndroidUtil {
-        return androidUtils
+        var instance = Holder.androidUtils.get()
+        if (instance == null) {
+            instance = AndroidUtil()
+            Holder.androidUtils = SoftReference(instance)
+        }
+        return instance
     }
 }
