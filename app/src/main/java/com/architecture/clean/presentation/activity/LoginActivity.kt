59dd0fbb34ba8e4f9baf43b76ce8cafc775dc720
awecea <awecea@gmail.com>
2018-10-24 11:55:58 +0300

    Simplify adding a fragment to an activity.

--- app/src/main/java/com/architecture/clean/presentation/activity/LoginActivity.kt
+++ app/src/main/java/com/architecture/clean/presentation/activity/LoginActivity.kt
@@ -1,19 +1,16 @@
 package com.architecture.clean.presentation.activity
 
 import android.os.Bundle
-import com.architecture.clean.R
 import com.architecture.clean.presentation.fragment.LoginFragment
 
 /**
- * A login screen.
+ * The login screen.
  */
 class LoginActivity : BaseActivity() {
-    override fun getLayoutId() = R.layout.activity_layout
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (savedInstanceState == null) {
-            addFragment(R.id.fragment_container, LoginFragment())
+            addFragment(android.R.id.content, LoginFragment())
         }
     }
 }
