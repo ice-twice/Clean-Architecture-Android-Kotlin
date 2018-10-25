52b77415d7c61b426167d5ea507a58181aa02a11
awecea <awecea@gmail.com>
2018-10-25 21:25:13 +0300

    Initialize StrictMode.

--- app/src/main/java/com/architecture/clean/presentation/AndroidApplication.kt
+++ app/src/main/java/com/architecture/clean/presentation/AndroidApplication.kt
@@ -1,6 +1,7 @@
 package com.architecture.clean.presentation
 
 import android.app.Application
+import android.os.StrictMode
 import com.architecture.clean.BuildConfig
 import com.architecture.clean.presentation.di.ModuleProvider
 import com.squareup.leakcanary.LeakCanary
@@ -11,6 +12,19 @@ class AndroidApplication : Application() {
     }
 
     override fun onCreate() {
+        // initialize strict mode
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyFlashScreen()
+                    .build())
+            StrictMode.setVmPolicy(StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build())
+        }
         super.onCreate()
         // start to detect memory leaks
         if (BuildConfig.DEBUG) {
