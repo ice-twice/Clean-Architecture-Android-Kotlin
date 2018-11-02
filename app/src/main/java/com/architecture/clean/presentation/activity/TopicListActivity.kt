501871e65a5a6cbcbac486d4edcfcb008c6b46ef
awecea <awecea@gmail.com>
2018-11-02 16:35:00 +0200

    Add the title of the toolbar. Set the click listener on the home button.

--- app/src/main/java/com/architecture/clean/presentation/activity/TopicListActivity.kt
+++ app/src/main/java/com/architecture/clean/presentation/activity/TopicListActivity.kt
@@ -1,6 +1,7 @@
 package com.architecture.clean.presentation.activity
 
 import android.os.Bundle
+import android.view.MenuItem
 import com.architecture.clean.R
 import com.architecture.clean.presentation.fragment.TopicListFragment
 import kotlinx.android.synthetic.main.toolbar.*
@@ -16,5 +17,15 @@ class TopicListActivity : BaseActivity() {
             addFragment(R.id.fragment_container, TopicListFragment())
         }
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = getString(R.string.topics)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean = when (item?.itemId) {
+        android.R.id.home -> {
+            onBackPressed()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
     }
 }
