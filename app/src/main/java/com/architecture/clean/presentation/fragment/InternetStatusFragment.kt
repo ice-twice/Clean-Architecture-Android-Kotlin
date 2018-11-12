65324e92350d3a6e3ff8d8a2f62cefa2af91bd30
awecea <awecea@gmail.com>
2018-11-12 17:36:54 +0200

    Add the internet status view.

---
+++ app/src/main/java/com/architecture/clean/presentation/fragment/InternetStatusFragment.kt
@@ -0,0 +1,7 @@
+package com.architecture.clean.presentation.fragment
+
+import com.architecture.clean.R
+
+class InternetStatusFragment: BaseFragment() {
+    override fun layoutId(): Int  = R.layout.fragment_internet_status
+}
