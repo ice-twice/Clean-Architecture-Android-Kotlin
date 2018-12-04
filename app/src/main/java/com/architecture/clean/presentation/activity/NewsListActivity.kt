d105d1978de93e77371bdb50619449ba86dfe1e8
awecea <awecea@gmail.com>
2018-12-04 22:34:57 +0200

    Add the news list view.

---
+++ app/src/main/java/com/architecture/clean/presentation/activity/NewsListActivity.kt
@@ -0,0 +1,17 @@
+package com.architecture.clean.presentation.activity
+
+import androidx.fragment.app.Fragment
+import com.architecture.clean.R
+import com.architecture.clean.presentation.fragment.TopicListFragment
+
+/**
+ * The news list activity.
+ */
+class NewsListActivity : BaseActivity() {
+    override fun needSetContentView(): Boolean = true
+    override fun layoutId(): Int = R.layout.activity_layout
+    override fun fragmentContainerId(): Int = R.id.fragment_container
+    override fun fragment(): Fragment = TopicListFragment()
+    override fun initializeToolbar(): Boolean = true
+    override fun toolbarTitle(): String = getString(R.string.news)
+}
---
+++ app/src/main/java/com/architecture/clean/presentation/fragment/NewsListFragment.kt
@@ -0,0 +1,10 @@
+package com.architecture.clean.presentation.fragment
+
+import com.architecture.clean.R
+
+/**
+ * The news list fragment.
+ */
+class NewsListFragment : BaseFragment() {
+    override fun layoutId(): Int = R.layout.fragment_news_list
+}
