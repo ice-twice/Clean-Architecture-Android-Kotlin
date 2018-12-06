55857a0676a81616ea26297bd63cd72b56378c19
awecea <awecea@gmail.com>
2018-12-06 18:36:04 +0200

    Reformat.

--- app/src/main/java/com/architecture/clean/domain/interactor/abstractinteractor/adapter/stream/BaseStreamAdapter.kt
+++ app/src/main/java/com/architecture/clean/domain/interactor/abstractinteractor/adapter/stream/BaseStreamAdapter.kt
@@ -8,7 +8,7 @@ import io.reactivex.disposables.Disposable
  * The base adapter for different streams from the reactive library.
  * This class is used to create the base class for different streams.
  */
-abstract class BaseStreamAdapter<Observer>{
+abstract class BaseStreamAdapter<Observer> {
 
     abstract fun subscribeOn(scheduler: Scheduler): BaseStreamAdapter<Observer>
     abstract fun observeOn(scheduler: Scheduler): BaseStreamAdapter<Observer>
